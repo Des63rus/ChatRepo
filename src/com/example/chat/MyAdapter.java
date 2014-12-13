aa88cbdbd8e2d4ab5d7a1dd6d3da6b50f76f3a1f
Kolesnikov Andrey <akolesnikov@lanit.ru>
2014-12-14 00:07:10 +0200

    14/12/ Прикрутил аватарку в Парс, Регистрацию и ЛистВьюАдаптер. Вроде Всё ок, всё работает

--- src/com/example/chat/MyAdapter.java
+++ src/com/example/chat/MyAdapter.java
@@ -1,6 +1,8 @@
 package com.example.chat;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +10,13 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.parse.GetDataCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
 import java.util.Vector;
 
@@ -17,7 +26,6 @@ import java.util.Vector;
 public class MyAdapter extends android.widget.BaseAdapter {
 
     private Context mContext;
-
 
 
     RelativeLayout rl;
@@ -53,23 +61,72 @@ public class MyAdapter extends android.widget.BaseAdapter {
 
 
         TextView tvMass = (TextView) rl.findViewById(R.id.tvMass);
-        tvMass.setText(Singleton.getInstance().getMessageVector().get(i).getFrom() + ": "+ Singleton.getInstance().getMessageVector().get(i).getMessage());
+        tvMass.setText(Singleton.getInstance().getMessageVector().get(i).getFrom() + ": " + Singleton.getInstance().getMessageVector().get(i).getMessage());
 
 
         TextView tvFrom = (TextView) rl.findViewById(R.id.tvFrom);
-        tvFrom.setText("TO: "+ Singleton.getInstance().getMessageVector().get(i).getTo());
+        tvFrom.setText("TO: " + Singleton.getInstance().getMessageVector().get(i).getTo());
 
 
         TextView tvTime = (TextView) rl.findViewById(R.id.tvTime);
-        tvTime.setText(Singleton.getInstance().getMessageVector().get(i).getDate().toString().substring(11,16));
+        tvTime.setText(Singleton.getInstance().getMessageVector().get(i).getDate().toString().substring(11, 16));
 
+        final ImageView ivAva = (ImageView) rl.findViewById(R.id.ivAvaChat);
+        for (int x = 0; x < Singleton.getInstance().getUsersVector().size(); x++) {
+            if (Singleton.getInstance().getUsersVector().get(i).getUsername().equals(Singleton.getInstance().getMessageVector().get(i).getFrom())) {
+                ParseFile imageFile = (ParseFile) Singleton.getInstance().getUsersVector().get(i).get("image");
+                imageFile.getDataInBackground(new GetDataCallback() {
+                    @Override
+                    public void done(byte[] bytes, ParseException e) {
+                        if (e == null) {
+                            Bitmap bmp = BitmapFactory
+                                    .decodeByteArray(
+                                            bytes, 0,
+                                            bytes.length);
+                            // Set the Bitmap into the
+                            // ImageView
+                            ivAva.setImageBitmap(bmp);
+                        } else {
+                            // something went wrong
+                        }
+                    }
+                });
 
+            }
+
+        }
+
+        if (Singleton.getInstance().getMessageVector().get(i).getFrom().equals(Singleton.getInstance().getUser().getUsername())) {
+            ParseFile imageFile = (ParseFile) Singleton.getInstance().getUser().get("image");
+            imageFile.getDataInBackground(new GetDataCallback() {
+                @Override
+                public void done(byte[] bytes, ParseException e) {
+                    if (e == null) {
+                        Bitmap bmp = BitmapFactory
+                                .decodeByteArray(
+                                        bytes, 0,
+                                        bytes.length);
+                        // Set the Bitmap into the
+                        // ImageView
+                        ivAva.setImageBitmap(bmp);
+                    } else {
+                        // something went wrong
+                    }
+                }
+            });
+
+        }
 
         return rl;
-
     }
 
 
 
 
+
+
 }
+
+
+
+
--- src/com/example/chat/Registr.java
+++ src/com/example/chat/Registr.java
@@ -2,15 +2,21 @@ package com.example.chat;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.*;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
+
+import java.io.ByteArrayOutputStream;
 
 
 public class Registr extends Activity {
@@ -28,7 +34,9 @@ public class Registr extends Activity {
     private int avatarka;
     private boolean gender;
     private boolean online;
-
+    private ImageView imageView;
+    private Bitmap imageBitmap;
+    static final int GALLERY_REQUEST = 1;
     public void registration(View view) {
         if (etLogin.getText().toString().trim().equals("")) {
             Toast.makeText(getApplicationContext(), "Введите логин", Toast.LENGTH_SHORT).show();
@@ -55,8 +63,8 @@ public class Registr extends Activity {
 
         try {
             user.signUp();
-        } catch (ParseException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("registr test foto"+e);;
         }
 
 
@@ -66,6 +74,17 @@ public class Registr extends Activity {
                 if (e != null) {
                     System.out.println(e);
                 } else {
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                    imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                    byte[] data = stream.toByteArray();
+                    ParseFile imgFile = new ParseFile(login+".png", data);
+                    imgFile.saveInBackground();
+
+                    user.put("image", imgFile);
+                    user.saveInBackground();
+
+
+
 
                     Singleton.getInstance().getUser().setObjectId(user.getObjectId());
                     Singleton.getInstance().setUser(user);
@@ -79,6 +98,54 @@ public class Registr extends Activity {
 
     }
 
+    public void camera ( View view) {Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, 0);
+        }
+
+    }
+
+    public void openGalery (View view) {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, GALLERY_REQUEST);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==0 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+
+        }
+
+        if (requestCode==1 && resultCode == RESULT_OK) {
+            imageBitmap = null;
+
+            switch(requestCode) {
+                case GALLERY_REQUEST:
+                    if(resultCode == RESULT_OK){
+                        Uri selectedImage = data.getData();
+                        try {
+                            imageBitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
+                        } catch (Exception e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                       imageView.setImageBitmap(imageBitmap);
+                    }
+            }
+        }
+
+
+
+
+        }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -89,6 +156,7 @@ public class Registr extends Activity {
         etPassReap = (EditText) findViewById(R.id.etRegPassReP);
         etMail = (EditText) findViewById(R.id.etRegMail);
         Spinner spGenfer = (Spinner) findViewById(R.id.spinerGender);
+        imageView = (ImageView) findViewById(R.id.ivRegAva);
 
         final ArrayAdapter<String> genderArr = new ArrayAdapter<String>(getApplication(), android.R.layout.simple_list_item_1, new String[]{"Муж", "Жен"});
 
